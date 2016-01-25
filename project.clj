33373cdc84a753e83bc00342d6b75f349c20bbbf
Ashton Kemerling <akemerling@drw.com>
2016-01-25 15:12:51 -0600

    Example of error messages

--- project.clj
+++ project.clj
@@ -3,7 +3,14 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]]
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [org.clojure/clojurescript "1.7.170"]]
   :main ^:skip-aot java-example.core
   :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
+  :profiles {:uberjar {:aot :all}}
+  :java-source-paths ["src/java"]
+  :plugins [[lein-cljsbuild "1.1.2"]]
+  :cljsbuild {:builds [{:source-paths ["src"]
+                        :compiler {:output-to "war/javascripts/main.js" ; default: target/cljsbuild-main.js
+                                   :optimizations :whitespace
+                                   :pretty-print true}}]})
--- src/java_example/core.clj
+++ src/java_example/core.clj
@@ -1,7 +1,8 @@
 (ns java-example.core
+  (import Foo)
   (:gen-class))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (println (.bar (Foo.))))
